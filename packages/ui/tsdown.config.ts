d0c84a9dc349e0f63e861e9e2bd6cc517a5310ff
lqf <maculosa@163.com>
2026-03-27 17:37:54 +0800

    feat(ui): 添加 Vue 插件支持并更新构建配置
    - 在 tstown 配置中添加 Vue 插件支持
    - 更新 package.json 添加构建相关脚本
    - 添加 unplugin-vue 依赖

--- packages/ui/tsdown.config.ts
+++ packages/ui/tsdown.config.ts
@@ -1,14 +1,21 @@
 import { defineConfig } from 'tsdown';
+import Vue from 'unplugin-vue/rolldown'
 
 export default defineConfig({
     entry: [
         'src/index.ts'
     ],
     format: ['esm', 'cjs'],
-    dts: true,
+    dts: {
+        vue: true
+    },
     clean: true,
     sourcemap: true,
     deps: {
         neverBundle: ['vue']
-    }
+    },
+    platform: 'neutral',
+    plugins: [
+        Vue({ isProduction: true })
+    ]
 })
